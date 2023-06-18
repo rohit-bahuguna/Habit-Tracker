e6e1cf14987d713ec77b5f51be30889e225e1766
rohit-bahuguna <rohitbahuguna.4446.com>
2023-06-18 21:29:56 +0530

    logic completed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,28 +6,31 @@ import HabitForm from '../components/HabitForm'
 const HomePage = () => {
     const { habits, toggle, setToggle } = useHabitData()
     return (
-        <div className='flex gap-10 flex-wrap p-10' >
-            <div className='flex  justify-between   border w-56 bg-gray-200'
-                onClick={() => {
-                    setToggle(true)
-                }}
-            >
-                <h1>Add New Habit</h1>
-                {/* <img src={image} alt={name} className='w-24' /> */}
-            </div>
-            {
-                habits && habits.map(({ id, name, image }) => <Link to={`habit-detail/${id}`}>
-                    <div className='flex  justify-between   border w-56 bg-gray-200'>
-                        <h1>{name}</h1>
-                        <img src={image} alt={name} className='w-24' />
-                    </div>
-                </Link>)
-            }
+        <>
+            <Link to="/archive">
+                <button className='border px-5 rounded-full  bg-sky-400 text-xl ' >Go To Archive</button></Link>
+            <div className='flex gap-10 flex-wrap p-10' >
+                <div className='flex  justify-between   border w-56 bg-gray-200'
+                    onClick={() => {
+                        setToggle(true)
+                    }}
+                >
+                    <h1>Add New Habit</h1>
+                    {/* <img src={image} alt={name} className='w-24' /> */}
+                </div>
+                {
+                    habits && habits.map(({ id, name, image }) => <Link to={`habit-detail/${id}`}>
+                        <div className='flex  justify-between   border w-56 bg-gray-200'>
+                            <h1>{name}</h1>
+                            <img src={image} alt={name} className='w-24' />
+                        </div>
+                    </Link>)
+                }
 
-            {
-                toggle && <HabitForm />
-            }
-        </div>
+                {
+                    toggle && <HabitForm />
+                }
+            </div></>
     )
 }
 
